6a404c389bd8f179d3247eefd79a0b20781d7e19
Tim <yangzair@outlook.com>
2023-12-19 15:57:18 +0800

    获取文件信息

---
+++ utils/file_info.py
@@ -0,0 +1,54 @@
+
+import os
+import json
+import re
+import time as t
+from unittest import result
+def traverse_folder(path, root_path, level=1, parent_path=None):
+    result = {
+        "Path": os.path.relpath(path, root_path),
+        "Name": os.path.basename(path),
+        "level": level,
+        "children": []
+    }
+
+    if parent_path:
+        result["level"] = level - len(parent_path.split(os.path.sep))
+
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+
+        if os.path.isdir(item_path): # 如果是文件夹
+            result["type"] = "folder"
+            result["children"].append(traverse_folder(item_path, parent_path, level + 1))
+        else: # 如果是文件
+            result["children"].append({
+                "Path": os.path.relpath(item_path, root_path),
+                "Name": item,
+                "type": os.path.splitext(item)[1][1:],
+                "level": level + 1,
+                "children": []
+            })
+
+    return result
+
+def get_tree(target_folder):
+    result = traverse_folder(target_folder,root_path=target_folder)
+    return json.dumps(result, indent=2,ensure_ascii=False)
+
+def get_tree_as_file(target_folder):
+    result = traverse_folder(target_folder,root_path=target_folder)
+    # file_name = str(t.time()) + ".json"
+    file_name = target_folder.split(os.path.sep)[-1] + ".json"
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)  # Get the absolute file path
+    with open(file_path,"w",encoding="utf-8") as f:
+        json.dump(result,f,indent=2,ensure_ascii=False)
+    print("File saved to: ", file_path)
+    return file_path
+
+
+if __name__ == "__main__":
+    target_folder = os.path.dirname(os.path.abspath(__file__))
+    # res = get_tree(target_folder)
+    # print(res)
+    get_tree_as_file(target_folder)
---
+++ utils/file_server.py
@@ -0,0 +1,37 @@
+from doctest import debug
+from flask import Flask, request, jsonify, render_template,send_file
+from file_info import get_tree, get_tree_as_file
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def api_docs():
+    return render_template('api_docs.html')
+
+@app.route('/get_tree')
+def get_tree_():
+    target_folder = request.args.get('target_folder')
+    return get_tree(target_folder)
+
+@app.route('/get_tree_as_file')
+def get_tree_as_file_():
+    target_folder = request.args.get('target_folder')
+    file_path = get_tree_as_file(target_folder)
+    print(file_path)
+    return send_file(file_path, as_attachment=True)
+
+@app.route('/get_tree_url')
+def get_tree_url():
+    target_folder = request.args.get('target_folder')
+    file_path = get_tree_as_file(target_folder)
+    file_name = os.path.basename(file_path)
+    return f"http://{request.host}/{file_name}"
+
+@app.route('/<path:filename>.json')
+def send_json_file(filename):
+    print(filename)
+    return send_file(filename + '.json')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
