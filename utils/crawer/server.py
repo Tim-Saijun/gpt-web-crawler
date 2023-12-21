935276d0654f1dddf90c12c96d343f13aa7b5088
Tim <yangzair@outlook.com>
2023-12-21 15:22:03 +0800

    Fix typo and update return value in craw_sigle_page_ function & Add some reference code about ai

--- utils/crawer/server.py
+++ utils/crawer/server.py
@@ -1,6 +1,6 @@
 from doctest import debug
 from flask import Flask, request, jsonify, render_template
-from craw_single_page import craw_sigle_page
+from craw_single_page import craw_single_page
 
 app = Flask(__name__)
 @app.route('/')
@@ -11,7 +11,10 @@ def api_docs():
 def craw_sigle_page_() -> str:  # 修改返回类型为str
     default_url = "https://www.leadong.com/"
     url = request.args.get('url', default_url)  # 'default_url' 是如果没有提供时的默认值
-    return craw_sigle_page(url)
+    result = craw_single_page(url)
+    if isinstance(result, list):
+        return result[1]
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
