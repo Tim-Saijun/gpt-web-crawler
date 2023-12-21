f96779edd38d81394bc8fbaa426b042af4901f52
Tim <yangzair@outlook.com>
2023-12-21 17:52:30 +0800

    可对模板自动提取正文

--- utils/crawer/travel_sitemap.py
+++ utils/crawer/travel_sitemap.py
@@ -24,12 +24,14 @@ def integrate_sitemap_with_details(sitemap):
         return [process_menu_item(menu_item) for menu_item in sitemap]
 
 # 获取原始站点地图
-original_sitemap = phar_sitemap_url('https://www.jiecang.cn/sitemap.html')[0]
+url = 'https://www.jiecang.cn/sitemap.html'
+original_sitemap = phar_sitemap_url(url)[0]
 # print(original_sitemap)
 # 整合站点地图与详细页面信息
 detailed_sitemap = integrate_sitemap_with_details(original_sitemap)
 
 # 输出结果为json文件
-with open(r'resources\sites\sitemap_jc.json', 'w',encoding='utf-8') as file:
-    json.dump(detailed_sitemap, file, ensure_ascii=False, indent=4)
+base_url = url.split('/')[2]
+with open(base_url + '.json', 'w',encoding='utf-8') as file:
+    json.dump(detailed_sitemap, file, ensure_ascii=False, indent=2)
 
