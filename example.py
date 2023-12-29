3fde241c8ede923ab49ea3702e9a1e28e86f425d
Tim <yangzair@outlook.com>
2023-12-29 13:19:59 +0800

    Publish

---
+++ example.py
@@ -0,0 +1,7 @@
+from gpt_web_crawler import run_spider
+from gpt_web_crawler.webcrawer import NoobSpider
+run_spider(NoobSpider, 
+           max_page_count= 10 ,
+           start_urls="https://www.jiecang.cn/", 
+           output_file = "test_pakages.json",
+           extract_rules= r'.*\.html' )
